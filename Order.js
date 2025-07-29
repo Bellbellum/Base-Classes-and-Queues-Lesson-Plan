b0f586d7847a3d67231d761aa1dde25fa6b62bbc
Sarah K <63137575+Bellbellum@users.noreply.github.com>
2025-07-28 19:03:20 -0700

    Update Order.js

--- Order.js
+++ Order.js
@@ -1,1 +1,20 @@
+class Order {
+    constructor(customerId, drinkType) {
+        this.customerId = customerId;
+        this.drinkType = drinkType;
+        this.preparationTime = this._getPreparationTime(drinkType);
+    }
 
+    getCustomerId() { return this.customerId; }
+    getDrinkType() { return this.drinkType; }
+    getPreparationTime() { return this.preparationTime; }
+
+    _getPreparationTime(drinkType) {
+        switch (drinkType) {
+            case 'coffee': return 5;
+            case 'tea': return 3;
+            case 'hot chocolate': return 7;
+            default: return 1; 
+        }
+    }
+}
