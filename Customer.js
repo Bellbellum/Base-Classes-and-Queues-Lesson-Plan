de00a8ffe93b660aa682db42786c8bccb07f7dce
Sarah K <63137575+Bellbellum@users.noreply.github.com>
2025-07-28 19:03:01 -0700

    Update Customer.js

--- Customer.js
+++ Customer.js
@@ -1,1 +1,24 @@
+class Customer {
+    constructor(id, arrivalTime, patience) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.patience = patience;   
+        this.order = null;
+    }
+
+    getId() { return this.id; }
+    getArrivalTime() { return this.arrivalTime; }
+    getPatience() { return this.patience; }
+    getOrder() { return this.order; }
+    setOrder(order) { this.order = order; }
+
+// time customer waiting
+    getTimeWaited(currentTime) {
+        return currentTime - this.arrivalTime;
+    }
+// Is customer still patient?
+    isPatient(currentTime) {
+        return this.getTimeWaited(currentTime)
+    }
+}
 
